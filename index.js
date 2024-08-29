e017d335b95acacdfdfce51289e14fb24f9508be
Kapil Gorve <kapilsg186@gmail.com>
2024-08-29 16:21:55 +0530

    add host

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fastify.get('/', async (request, reply) => {
 });
 
 try {
-    await fastify.listen({port: process.env.PORT ||80 });
+    await fastify.listen({port: process.env.PORT || 80, host: '0.0.0.0' });
 } catch (error) {
     fastify.log.error(error);
     process.exit(1);
